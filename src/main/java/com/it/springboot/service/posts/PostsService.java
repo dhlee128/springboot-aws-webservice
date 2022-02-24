4a900ad93fc0a3b021660b7b41781130aacddf22
dahee <daheewa128@gmail.com>
2022-02-24 23:36:53 +0900

    chap04. add delete API Thymeleaf 사용하여 화면 구성하기

--- src/main/java/com/it/springboot/service/posts/PostsService.java
+++ src/main/java/com/it/springboot/service/posts/PostsService.java
@@ -3,12 +3,16 @@ package com.it.springboot.service.posts;
 import com.it.springboot.domain.BaseTimeEntity;
 import com.it.springboot.domain.posts.Posts;
 import com.it.springboot.domain.posts.PostsRepository;
+import com.it.springboot.web.dto.PostsListResponseDto;
 import com.it.springboot.web.dto.PostsResponseDto;
 import com.it.springboot.web.dto.PostsSaveRequestDto;
 import com.it.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,4 +41,20 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
 }
---
+++ src/main/java/com/it/springboot/web/IndexController.java
@@ -0,0 +1,37 @@
+package com.it.springboot.web;
+
+import com.it.springboot.service.posts.PostsService;
+import com.it.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor //final 이나 @NonNull 이 붙은 필드에 대해 생성자를 생성해준다.
+@Controller
+public class IndexController {
+
+    // 생성자가 오직 하나이고,
+    // 생성자의 파라미터 타입이 빈으로 등록 가능한 존재라면 이 빈은 @Autowired 어노테이션 없이도 의존성 주입이 가능하다.
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
